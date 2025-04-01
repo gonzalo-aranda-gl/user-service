0b7fbd1674855339825c4ff991e74f6c4042217a
Gonzalo Aranda <gonzalo.aranda@globallogic.com>
2025-04-01 17:51:34 -0300

    - H2 database configuration

--- src/main/java/com/core/user_service/UserServiceApplication.java
+++ src/main/java/com/core/user_service/UserServiceApplication.java
@@ -3,7 +3,7 @@ package com.core.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.core.user-service"})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/core/user_service/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.core.user_service.api.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+}
