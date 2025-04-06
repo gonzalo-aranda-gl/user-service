4eb1503b48b17818f4c10d1584d79ded4781d8ae
Gonzalo Aranda <gonzalo.aranda@globallogic.com>
2025-04-06 17:31:18 -0300

    - Fixed password regular expression

--- src/main/java/com/core/user_service/constants/RegularExpressions.java
+++ src/main/java/com/core/user_service/constants/RegularExpressions.java
@@ -1,8 +1,11 @@
 package com.core.user_service.constants;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class RegularExpressions {
 
-  public static final String PASSWORD_FORMAT = "^(?=[^A-Z]*[A-Z][^A-Z]*$)(?=(.*\\\\d.*){1,2})(?=[a-z]).{8,12}$";
+  public static final String PASSWORD_FORMAT = "^(?=[^A-Z]*[A-Z][^A-Z]*$)(?=(?:\\D*\\d){2}\\D*$)[a-zA-Z\\d]{8,12}$";
 
   public static final String EMAIL_FORMAT = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}";
 }
